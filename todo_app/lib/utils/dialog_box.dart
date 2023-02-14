93254c31d8807dc8e633de6c028ff0129c924a53
Hasith435 <66883843+Hasith435@users.noreply.github.com>
2023-02-14 15:19:53 +0400

    Add task functionality
    Added the function to add a task

---
+++ todo_app/lib/utils/dialog_box.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+
+  DialogBox({super.key, required this.controller, required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.grey,
+      content: SizedBox(
+        height: 130,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //text entry field
+            TextFormField(
+              controller: controller,
+              decoration: InputDecoration(
+                  hintText: "Enter task name",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+
+            //date picker
+
+            //save and cancel button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                //save button
+                ElevatedButton(
+                  onPressed: onSave,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color.fromARGB(255, 130, 241, 133)),
+                  child: const Text('Save'),
+                ),
+
+                const SizedBox(
+                  width: 8,
+                ),
+
+                //cancel button
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 238, 111, 72)),
+                    child: const Text('Cancel'))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- todo_app/lib/utils/todo_list_container.dart
+++ todo_app/lib/utils/todo_list_container.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class ToDoList extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
+  VoidCallback onDel;
   Function(bool?)? onChanged;
 
   ToDoList(
       {super.key,
       required this.taskName,
       required this.taskCompleted,
+      required this.onDel,
       required this.onChanged});
 
   @override
@@ -21,11 +23,27 @@ class ToDoList extends StatelessWidget {
               borderRadius: BorderRadius.circular(16), color: Colors.grey[600]),
           child: Row(
             children: [
-              Checkbox(value: taskCompleted, onChanged: onChanged),
-              const Text(
-                'Code app',
-                style: TextStyle(color: Colors.white, fontSize: 16),
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
               ),
+              Text(
+                taskName,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                    decorationColor: Colors.black,
+                    decorationThickness: 2),
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+                color: Colors.white,
+              )
             ],
           ),
         ));
